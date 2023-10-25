c7308a83ac505ef45693d9f4d100d3d70d0426c0
masonishee <mishee2010@gmail.com>
2023-10-24 20:08:12 -0500

    Add login, signup, and logout

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,14 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
+
 export function Content() {
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <Signup />
+      <Login />
+      <LogoutLink />
     </div>
   );
 }
